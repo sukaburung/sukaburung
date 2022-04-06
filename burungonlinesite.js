8d5f391d0ff3b39e606a5e89de474eaaffea1cd8
sukaburung <103101995+sukaburung@users.noreply.github.com>
2022-04-06 15:34:11 +0700

    Update burungonlinesite.js

--- burungonlinesite.js
+++ burungonlinesite.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*3)+1);var xxx=null;if(x=="1"){xxx="how-much-equity-can-i-borrow.html"}
+{var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*2)+1);var xxx=null;if(x=="1"){xxx="how-much-equity-can-i-borrow.html"}
 if(x=="2"){xxx="real-estate-investment.html"}
 if(x=="3"){xxx="advocate-online.html"}a_to_ve[i].href="https://sukaburungindo.blogspot.com/2022/04/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
